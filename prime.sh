8093121408ba607764742de248febe07c1b57088
krishnanjali6778 <anjalikrishnaptb74@gmail.com>
2019-04-03 10:43:43 +0530

    This is my first commit

---
+++ prime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "enter the number:"
+read n
+i=2
+f=0
+while test $i -le `expr $n / 2`
+do
+if test `expr $n % $i` -eq 0
+then
+f=1
+fi
+i=`expr $i + 1` 
+done
+if test $f -eq 1
+then
+echo "It is not a prime number"
+else
+echo "It is a prime number"
+fi
